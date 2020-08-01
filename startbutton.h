49a0d19f35d9e61a0f8660735a6f5d4f630fb91c
chenjiaqiang-a <2504919775@qq.com>
2020-08-01 16:08:25 +0800

    Create startbutton.h

---
+++ startbutton.h
@@ -0,0 +1,16 @@
+#ifndef STARTBUTTON_H
+#define STARTBUTTON_H
+
+#include <QPushButton>
+
+class StartButton : public QPushButton
+{
+    Q_OBJECT
+public:
+    explicit StartButton(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // STARTBUTTON_H
